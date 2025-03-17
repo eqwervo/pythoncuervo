e2d3802d66f85daf7c0324147a3dd3de4988fa0c
esteban cuervo <cuervoesteban87@gmail.comm>
2025-03-17 01:20:57 -0300

    correccion input usuario

--- questions.py
+++ questions.py
@@ -36,16 +36,22 @@ for _ in range(3):
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
-        # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
-            print("¡Correcto!")
-            break
-    else:
-        # Si el usuario no responde correctamente después de 2 intentos,
-        # se muestra la respuesta correcta
-        print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        try:
+            user_answer = int(input("Respuesta: ")) - 1
+            if 0 <= user_answer < len(answers[question_index]):
+            # Se verifica si la respuesta es correcta
+                if user_answer == correct_answers_index[question_index]:
+                    print("¡Correcto!")
+                    break
+                else:
+                    # Si el usuario no responde correctamente después de 2 intentos,
+                    # se muestra la respuesta correcta
+                    print("Incorrecto. La respuesta correcta es:")
+                    print(answers[question_index][correct_answers_index[question_index]])
+            else: 
+                print("Ingrese una respuesta válida.")
+        except:
+            print("Ingrese una respuesta válida.")
 
     # Se imprime un blanco al final de la pregunta
     print()
