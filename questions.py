c08ad21e195acbb2f4470cfd1048f7953a9d80e1
esteban cuervo <cuervoesteban87@gmail.comm>
2025-03-17 10:14:31 -0300

    Reemplazo acceso a listas: ind>>var

--- questions.py
+++ questions.py
@@ -23,39 +23,50 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
-user_points = float(0)
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
 
+# union de cada conjunto pregunta, respuesta, indice
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+
+# puntuaje del usuario
+user_points = float(0)
+
+# El usuario deberá contestar 3 preguntas. Se reemplaza el range por un for 
+# que itera sobre el conjunto (Preg,Res,Ind) que fueron unidas en el zip
+for quest, answ, ind in questions_to_ask:
+    
+    #question_index = random.randint(0, len(questions) - 1)
+    
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(quest)
+    for i,answer in enumerate(answ):
         print(f"{i + 1}. {answer}")
 
     # El usuario tiene 2 intentos para responder correctamente
     
     for intento in range(2):
+        # try con una primera ocurrencia donde ingresa un int, en caso que ingrese otro valor va al except
         try:
             user_answer = int(input("Respuesta: ")) - 1
-            if 0 <= user_answer < len(answers[question_index]):
+            if 0 <= user_answer < len(answ):
             # Se verifica si la respuesta es correcta
-                if user_answer == correct_answers_index[question_index]:
+                if user_answer == ind:
                     print("¡Correcto!")
                     user_points += 1
                     break
-                else:
-                    # Si el usuario no responde correctamente después de 2 intentos,
-                    # se muestra la respuesta correcta
-                    print("Incorrecto. La respuesta correcta es:")
-                    user_points += 0.5
-                print(answers[question_index][correct_answers_index[question_index]])
             else: 
                 print("Ingrese una respuesta válida.")
         except:
             print("Ingrese una respuesta válida.")
+            
+# si no logra en dos intentos responder correctamente entra en el else
+    else:
+                    # Si el usuario no responde correctamente después de 2 intentos,
+                    # se muestra la respuesta correcta
+                    print("Incorrecto. La respuesta correcta es:")
+                    user_points += 0.5
+                    print(answ[ind])
 
+    
     # Se imprime un blanco al final de la pregunta
     print()
 print(f"Su puntaje fue:  {user_points}")
